009b0bbdb92287900ef8fe2ef03d19b1ac7e9c64
zh8 <zh8@meitu.com>
2020-07-03 22:28:33 +0800

    【feature】路由配置

--- server/src/client/index.js
+++ server/src/client/index.js
@@ -1,8 +1,17 @@
 // 在浏览器执行
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 
-import Home from '../containers/Home/index'
+import Routes from '../Routes'
 
+// 客户端路由配置
+const App = () => {
+  return (
+    <BrowserRouter>
+      { Routes }
+    </BrowserRouter>
+  )
+}
 // ReactDOM.render(<Home />, document.getElementById('root'))
-ReactDOM.hydrate(<Home />, document.getElementById('root'))
+ReactDOM.hydrate(<App />, document.getElementById('root'))
--- server/src/server/index.js
+++ server/src/server/index.js
@@ -2,26 +2,34 @@
 import express from 'express'
 import React from 'react'
 import { renderToString } from 'react-dom/server'
-
-import Home from '../containers/Home/index'
+import { StaticRouter } from 'react-router-dom'
+import Routes from '../Routes'
 // 虚拟dom  =》 JavaScript 对象的映射
 // 加快首屏渲染  利于seo
 // 缺点 大量消耗服务器性能，浪费不必要的服务器性能
 const app = express()
 const port = 3000
 app.use(express.static('public'))
-const content = renderToString(<Home />)
 
-app.get('/', (req, res) => res.send(
+// 服务器路由配置
+app.get('/', (req, res) => {
+  const content = renderToString((
+    <StaticRouter context={{}} location={req.path}>
+      {Routes}
+    </StaticRouter>
+  ))
+  res.send(
+    `
+      <html>
+      <title>ssr</title>
+      <body>
+        <div id="root">${content}</div>
+        <script src="/index.js"></script>
+      </body>
+      </html>
   `
-    <html>
-    <title>ssr</title>
-    <body>
-      <div id="root">${content}</div>
-      <script src="/index.js"></script>
-    </body>
-    </html>
- `
-))
+  )
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
