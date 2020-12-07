ae27f11beaa00d7fddcfb5d2c52be6aaabd9ffe7
zh8 <zh8@meitu.com>
2020-12-07 17:29:27 +0800

    no message

---
+++ react-ssr/build/webpack.devClient.config.js
@@ -0,0 +1,32 @@
+/*
+ * @description webpack 开发环境配置
+ * @fileName webpack.dev.config.js
+ * @author zh8
+ * @date 2020/10/26 13:37:36
+*/
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve } = require('../utils');
+const merge = require('webpack-merge');
+
+const webpack = require('webpack');
+
+const baseConfig = require('./webpack.base.config');
+
+
+module.exports = merge(baseConfig, {
+  // devtool: 'inline-source-map',
+  mode: 'development',
+  devServer: {
+    host: '0.0.0.0',
+    port: 8001,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify('development')
+    }),
+      new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: resolve('template/app.html')
+        })
+  ]
+})
---
+++ react-ssr/readme.md
@@ -0,0 +1,12 @@
+ ### 
+ npm install
+
+
+ ##### 启动单页应用开发模式
+ npm run dev1:start
+ 端口： 8001
+
+ ##### 启动node ssr 服务
+ npm run dev
+ 端口： 9000
+
